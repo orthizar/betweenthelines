e434f4a2c3eb6879092faeca2f5d60d6902666e3
Luca Mock <luca.mock@ringieraxelspringer.ch>
2023-10-03 10:09:24 -0700

    base project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import ChatGPT from "./components/ChatGPT";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="MainContainer">
+        <ChatGPT />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ChatGPT/index.js
@@ -0,0 +1,106 @@
+import "./styles.css";
+
+import React, { useState } from "react";
+
+import { BsCheckLg } from "react-icons/bs";
+import { GoCopy } from "react-icons/go";
+import { RiDeleteBin6Line } from "react-icons/ri";
+// import { GrammarlyEditorPlugin } from "@grammarly/editor-sdk-react";
+import axios from "axios";
+
+const ChatGPT = () => {
+  const [gptResponse, setGptResponse] = useState(null);
+  const [inputValue, setInputValue] = useState();
+  const [isCopied, setIsCopied] = useState(false);
+
+  const sendGptRequest = async (inputText, improvementType) => {
+    const apiKey = "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";
+    const apiUrl =
+      "https://api.openai.com/v1/engines/text-davinci-003/completions";
+
+    try {
+      const response = await axios.post(
+        apiUrl,
+        {
+          prompt: `Make this text ${improvementType} and correct all spelling mistakes : ${inputText}`,
+          max_tokens: 50,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const gptResponse = response.data.choices[0].text;
+      setGptResponse(gptResponse);
+      setInputValue(gptResponse);
+    } catch (error) {
+      console.error("Fehler bei der API-Anfrage:", error);
+    }
+  };
+
+  const buttons = [
+    { id: "improve-button", label: "Improve", color: "#5f76a5" },
+    { id: "professional-button", label: "Professional", color: "#4c7937" },
+    { id: "colloquially-button", label: "Colloquially", color: "#794e37" },
+    { id: "persuasive-button", label: "Persuasive", color: "#374c79" },
+  ];
+
+  const ButtonGroup = () =>
+    buttons.map((button) => (
+      <button
+        key={button.id}
+        onClick={(event) => handleSubmit(event, button.label)}
+        className="textStyleButton"
+        style={{ backgroundColor: button.color }}
+      >
+        {button.label}
+      </button>
+    ));
+
+  const handleSubmit = (event, improvementType) => {
+    event.preventDefault();
+    sendGptRequest(inputValue, improvementType);
+  };
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    setGptResponse("");
+    setInputValue("");
+    setIsCopied(false);
+  };
+
+  const handleCopy = (event) => {
+    event.preventDefault();
+    navigator.clipboard.writeText(gptResponse).then(() => {
+      setIsCopied(true);
+    });
+  };
+
+  return (
+    <div>
+      <div className="box">
+        <div className="rectangle">
+          <textarea
+            onChange={(event) => setInputValue(event.target.value)}
+            placeholder="Enter your text here"
+            value={inputValue}
+          ></textarea>
+          <div className="greyBox">
+            <div className="buttonContainer">
+              <ButtonGroup />
+            </div>
+            <div className="iconContainer">
+              {isCopied ? <BsCheckLg /> : <GoCopy onClick={handleCopy} />}
+              <RiDeleteBin6Line onClick={handleDelete} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatGPT;
