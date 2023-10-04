afdf1dcb7f115e031f2512a77bc214c1ca70a339
AP\aport <ap05@gmx.ch>
2023-10-04 10:05:36 -0700

    Chatbot implemented

--- src/components/ChatGPT/index.js
+++ src/components/ChatGPT/index.js
@@ -1,46 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 import { BsCheckLg } from "react-icons/bs";
 import { GoCopy } from "react-icons/go";
 import { RiDeleteBin6Line } from "react-icons/ri";
 // import { GrammarlyEditorPlugin } from "@grammarly/editor-sdk-react";
 import axios from "axios";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import { correctionsPrompt } from "./prompts";
 
 const ChatGPT = () => {
-  const [formattedValue, setFormattedValue] = useState();
+  const [gptResponse, setGptResponse] = useState(null);
+  const [inputValue, setInputValue] = useState();
   const [isCopied, setIsCopied] = useState(false);
-  const [gptCorrections, setGptCorrections] = useState([]);
-  const editorRef = React.useRef(null);
 
-  const clearHighlighting = () => {
-    editorRef.current.editor.removeFormat(0, editorRef.current.editor.getLength()-1);
+  const [chatMessages, setChatMessages] = useState([
+    {id: 1, author: 'User', text: 'Hallo! Wie geht es Ihnen?'},
+    {id: 2, author: 'Ich', text: 'Mir geht es gut. Danke!'}
+  ]);
+
+  const [message, setMessage] = useState('');
+
+  const isMyMessage = (author) => author === 'Ich';
+
+  const sendMessage = () => {
+    if (message.trim() !== '') {
+      const newMessage = {
+        id: chatMessages.length + 1,
+        author: 'Ich',
+        text: message
+      };
+      sendChatGptRequest(newMessage.text, inputValue);
+      setChatMessages([...chatMessages, newMessage]);
+      setMessage('');
+    }
   };
 
-  const highlightCorrections = (corrections) => {
-    corrections.forEach(correction => {
-      editorRef.current.editor.formatText(correction.start, correction.end - correction.start, { "color": "red" });
-    });
-  };
-
-  const selectCorrection = (correction) => {
-    editorRef.current.editor.removeFormat(correction.start, correction.end - correction.start)
-    editorRef.current.editor.formatText(correction.start, correction.end - correction.start, { "background": "red" });
-  };
-
-  const sendGptRequest = async (inputText, improvementType) => {
+  const sendChatGptRequest = async(inputChat, inputText ) => {
     const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
-    const apiUrl =
-      "https://api.openai.com/v1/completions";
-    if (improvementType === "Correct") {
-      try {
-        const response = await axios.post(
+    const apiUrl = "https://api.openai.com/v1/completions";
+
+    try {
+      const response = await axios.post(
           apiUrl,
           {
             model: "gpt-3.5-turbo-instruct",
-            prompt: 'Please output a json object with an array of all the mistakes in the text. Allowed types are "style", "spelling", "grammar". Use format with character range: { "corrections": [ { "index": 0, "length": 5, "correction": "Hello", "type": "spelling", "explanation": "Typo, change to \'Hello\'" } ] }. Text: \n"' + inputText + '"\n\nThe array should be empty if there are no mistakes. Do not output anything else than JSON. Valid JSON Output: \n',
+            prompt: `This is my current Context: ${inputText}, Please do this Command: ${inputChat}, give me the Mail Body back`,
             max_tokens: 200,
           },
           {
@@ -49,22 +51,24 @@ const ChatGPT = () => {
               "Content-Type": "application/json",
             },
           }
-        );
-        const responseData = await response.data.choices[0].text;
-        console.log(responseData);
-        console.log(JSON.parse(responseData));
-        setGptCorrections(JSON.parse(responseData).corrections);
-        // format quill editor text with corrections
-        JSON.parse(responseData).corrections.forEach(correction => {
-          editorRef.current.editor.formatText(correction.index, correction.length, { "color": "red" });
-        });
-      } catch (error) {
-        console.error("Fehler bei der API-Anfrage:", error);
-      }
+      );
+
+      const gptResponse = response.data.choices[0].text;
+      setGptResponse(gptResponse);
+      setInputValue(gptResponse);
+    } catch (error) {
+      console.error("Fehler bei der API-Anfrage:", error);
     }
-    else {
-      try {
-        const response = await axios.post(
+
+  };
+
+  const sendGptRequest = async (inputText, improvementType) => {
+    const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+    const apiUrl =
+        "https://api.openai.com/v1/completions";
+
+    try {
+      const response = await axios.post(
           apiUrl,
           {
             model: "gpt-3.5-turbo-instruct",
@@ -77,149 +81,120 @@ const ChatGPT = () => {
               "Content-Type": "application/json",
             },
           }
-        );
+      );
 
-        const gptResponse = response.data.choices[0].text;
-        setFormattedValue(gptResponse);
-      } catch (error) {
-        console.error("Fehler bei der API-Anfrage:", error);
-      }
+      const gptResponse = response.data.choices[0].text;
+      setGptResponse(gptResponse);
+      setInputValue(gptResponse);
+    } catch (error) {
+      console.error("Fehler bei der API-Anfrage:", error);
     }
   };
 
   const buttons = [
-    { id: "improve-button", label: "Improve", color: "bg-blue-600 hover:bg-blue-700" },
-    { id: "professional-button", label: "Professional", color: "bg-green-600 hover:bg-green-700" },
-    { id: "colloquially-button", label: "Colloquially", color: "bg-orange-600 hover:bg-orange-700" },
-    { id: "persuasive-button", label: "Persuasive", color: "bg-indigo-600 hover:bg-indigo-700" }, ,
-    { id: "correct-button", label: "Correct", color: "bg-red-600 hover:bg-red-700" },
+    {id: "improve-button", label: "Improve", color: "bg-blue-400 hover:bg-blue-700"},
+    {id: "professional-button", label: "Professional", color: "bg-green-400 hover:bg-green-700"},
+    {id: "colloquially-button", label: "Colloquially", color: "bg-orange-400 hover:bg-orange-700"},
+    {id: "persuasive-button", label: "Persuasive", color: "bg-indigo-400 hover:bg-indigo-700"},
   ];
 
   const ButtonGroup = () =>
-    buttons.map((button) => (
-      <button
-        key={button.id}
-        onClick={(event) => handleSubmit(event, button.label)}
-        className="textStyleButton"
-        style={{ backgroundColor: button.color }}
-      >
-        {button.label}
-      </button>
-    ));
+      buttons.map((button) => (
+          <button
+              key={button.id}
+              onClick={(event) => handleSubmit(event, button.label)}
+              className="textStyleButton"
+              style={{backgroundColor: button.color}}
+          >
+            {button.label}
+          </button>
+      ));
 
   const handleSubmit = (event, improvementType) => {
     event.preventDefault();
-    sendGptRequest(editorRef.current.editor.getText(), improvementType);
+    sendGptRequest(inputValue, improvementType);
   };
 
   const handleDelete = (event) => {
     event.preventDefault();
-    setFormattedValue("");
+    setGptResponse("");
+    setInputValue("");
     setIsCopied(false);
   };
 
   const handleCopy = (event) => {
     event.preventDefault();
-    navigator.clipboard.writeText(editorRef.current.editor.getText()).then(() => {
+    navigator.clipboard.writeText(gptResponse).then(() => {
       setIsCopied(true);
     });
   };
 
-  var modules = {
-    toolbar: null,
-
-  }
-  const getCorrection = (index) => {
-    for (var i = 0; i < gptCorrections.length; i++) {
-      if (index >= gptCorrections[i].start && index <= gptCorrections[i].end) {
-        return gptCorrections[i];
-      }
-    }
-    return null;
-  }
   return (
-    <div className="p-12 bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="bg-white shadow-xl p-8 rounded-lg w-4/5 max-w-4xl flex flex-col">
-        <div className="mb-6 flex-grow">
-          <ReactQuill
-            ref={editorRef}
-            theme="snow"
-            placeholder="Enter your text here"
-            value={formattedValue}
-            className="w-full p-4 border rounded-md resize-none text-lg"
-            onChange={(value, delta, source, editor) => {
-              console.log(value, source);
-              if (source === "user") {
-                const correction = getCorrection(delta.ops[0].retain)
-                console.log(correction);
-                if (correction != null) {
-                  editorRef.current.editor.removeFormat(correction.start, correction.end - correction.start)
-                  // delete the correction and move all following corrections
-                  setGptCorrections(gptCorrections.filter((item) => item !== correction));
-                  setGptCorrections(gptCorrections.map((item) => {
-                    var index = 0;
-                    var indexDelta = 0;
-                    delta.ops.forEach((op) => {
-                      if (op.retain) {
-                        index += op.retain;
-                      } else if (op.insert) {
-                        index += op.insert.length;
-                        indexDelta += op.insert.length;
-                      } else if (op.delete) {
-                        index -= op.delete;
-                        indexDelta -= op.delete;
-                      }
-                    });
-                    item.start -= indexDelta;
-                    item.end -= indexDelta;
-                    return item;
-                  }));
-                }
-              } else {
-                setFormattedValue(value);
-              }
-            }}
-            onChangeSelection={(selection, source, editor) => {
-              console.log(selection, source);
-              if (source === "user") {
-                if (selection != null && selection.length === 0) {
-                  const correction = getCorrection(selection.index);
-                  if (correction != null) {
-                    selectCorrection(correction);
-                  } else {
-                    clearHighlighting();
-                    highlightCorrections(gptCorrections);
-                  }
-                }
-              }
-            }}
-            modules={modules}
-          />
-        </div>
-        <div className="flex justify-between items-center">
-          <div className="space-x-4">
-            {buttons.map((button) => (
-              <button
-                key={button.id}
-                onClick={(event) => handleSubmit(event, button.label)}
-                className={`text-white py-2 px-5 rounded text-lg ${button.color}`}
-              >
-                {button.label}
-              </button>
-            ))}
+      <div className="p-12 bg-gray-100 min-h-screen flex items-center justify-center">
+        <div className="flex w-3/4 max-w-6xl h-[37rem]">
+          {/* Chatbot-Bereich */}
+          <div className="bg-white shadow-xl p-8 rounded-lg w-2/5 flex flex-col mr-6 max-h-[37rem] overflow-y-auto">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-xl font-bold">Chat</h2>
+            </div>
+            <div className="mb-6 flex-grow overflow-y-auto">
+              {chatMessages.map(chatMessage => (
+                  <div key={chatMessage.id}
+                       className={`p-3 rounded-lg mb-3 ${isMyMessage(chatMessage.author) ? "bg-blue-200 text-right" : "bg-gray-200"}`}>
+                    <p>{chatMessage.text}</p>
+                  </div>
+              ))}
+            </div>
+            <div className="mt-auto">
+                    <textarea
+                        onChange={(event) => setMessage(event.target.value)}
+                        placeholder="Nachricht eingeben..."
+                        value={message}
+                        className="w-full p-2 border rounded-md resize-none mb-2"
+                        rows="3"
+                    ></textarea>
+              <button onClick={sendMessage} className="w-full bg-blue-500 text-white p-2 rounded">Senden</button>
+            </div>
           </div>
-          <div className="space-x-4 flex items-center">
-            {isCopied ? (
-              <BsCheckLg className="text-green-600" size={28} />
-            ) : (
-              <GoCopy className="text-blue-600 cursor-pointer" size={28} onClick={handleCopy} />
-            )}
-            <RiDeleteBin6Line className="text-red-600 cursor-pointer" size={28} onClick={handleDelete} />
+
+          {/* Hauptinhalt */}
+          <div className="bg-white shadow-xl p-8 w-2/5 rounded-lg flex-grow flex flex-col">
+            <div className="mb-6 flex-grow">
+        <textarea
+            onChange={(event) => setInputValue(event.target.value)}
+            placeholder="Hier Text eingeben..."
+            value={inputValue}
+            className="w-full p-4 border rounded-md resize-none text-lg"
+            rows="15"
+        ></textarea>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="space-x-4">
+                {buttons.map((button) => (
+                    <button
+                        key={button.id}
+                        onClick={(event) => handleSubmit(event, button.label)}
+                        className={`text-white py-1 px-2 rounded text-lg ${button.color}`}
+                    >
+                      {button.label}
+                    </button>
+                ))}
+              </div>
+              <div className="space-x-4 flex items-center">
+                {isCopied ? (
+                    <BsCheckLg className="text-green-600" size={28} />
+                ) : (
+                    <GoCopy className="text-blue-600 cursor-pointer" size={28} onClick={() => {
+                      navigator.clipboard.writeText(inputValue);
+                      setIsCopied(true);
+                    }}/>
+                )}
+                <RiDeleteBin6Line className="text-red-600 cursor-pointer" size={28} onClick={() => setInputValue('')}/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
-};
-
+}
 export default ChatGPT;
