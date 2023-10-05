33bdc0fff9e7b721845700b922566b2e5487259d
Luca Mock <luca.mock@ringieraxelspringer.ch>
2023-10-05 09:48:15 -0700

    depoyement & button fix

--- src/components/ButtonGroup/index.js
+++ src/components/ButtonGroup/index.js
@@ -1,6 +1,34 @@
-import buttons from "./buttons.json";
+import React from "react";
 
 const ButtonGroup = ({ handleSubmit }) => {
+  const buttons = [
+    {
+      id: "improve-button",
+      label: "Improve",
+      color: "bg-blue-400 hover:bg-blue-700",
+    },
+    {
+      id: "professional-button",
+      label: "Professional",
+      color: "bg-green-400 hover:bg-green-700",
+    },
+    {
+      id: "colloquially-button",
+      label: "Colloquially",
+      color: "bg-orange-400 hover:bg-orange-700",
+    },
+    {
+      id: "persuasive-button",
+      label: "Persuasive",
+      color: "bg-indigo-400 hover:bg-indigo-700",
+    },
+    {
+      id: "correct-button",
+      label: "Correct",
+      color: "bg-red-600 hover:bg-red-700",
+    },
+  ];
+
   return (
     <div className="space-x-4">
       {buttons.map((button) => (
