7a15c960559c3a088c9363b31ed636c32f7b2ea5
Silvan Kohler <silvankohler@proton.me>
2023-10-06 12:55:23 -0700

    fix selection and correction behaviour

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -9,75 +9,21 @@ import nspell from 'nspell';
 var lang = 'en_US';
 const aff = await fetch(`dictionaries/${lang}/${lang}.aff`).then((r) => r.text());
 const dic = await fetch(`dictionaries/${lang}/${lang}.dic`).then((r) => r.text());
-console.log(aff, dic)
-const spell = nspell({aff: aff, dic: dic});
+const spell = nspell({ aff: aff, dic: dic });
 
 
 const Editor = ({ editorRef, formattedValue, setFormattedValue }) => {
-  const [spellCheckCorrections, setSpellCheckCorrections] = useState([]);
-  const [currentCorrection, setCurrentCorrection] = useState(null);
-  const [editorSuggestions, setEditorSuggestions] = useState([]);
+  const [spellCheckMistakes, setSpellCheckMistakes] = useState([]);
+  const [currentMistake, setCurrentMistake] = useState(null);
+  const [editorCorrections, setEditorCorrections] = useState([]);
 
 
   const editorModules = {
     toolbar: null,
   };
 
-  const getSpellCheckCorrection = (index) => {
-    for (var i = 0; i < spellCheckCorrections.length; i++) {
-      if (index >= spellCheckCorrections[i].start && index <= spellCheckCorrections[i].end) {
-        return spellCheckCorrections[i];
-      }
-    }
-    return null;
-  }
-
-  const clearHighlighting = () => {
-    editorRef.current.editor.removeFormat(
-      0,
-      editorRef.current.editor.getLength() - 1,
-      "silent"
-    );
-  };
-
-  const highlightCorrections = (corrections) => {
-    corrections.forEach((correction) => {
-      editorRef.current.editor.formatText(
-        correction.start,
-        correction.end - correction.start,
-        { color: "red" },
-        "silent"
-      );
-    });
-  };
-
-  const selectCorrection = (correction) => {
-    if (correction !== currentCorrection) {
-      editorRef.current.editor.removeFormat(
-        correction.start,
-        correction.end - correction.start,
-        "silent"
-      );
-      editorRef.current.editor.formatText(
-        correction.start,
-        correction.end - correction.start,
-        { background: "red" },
-        "silent"
-      );
-      setCurrentCorrection(correction);
-      setEditorSuggestions(getSuggestions(correction));
-    }
-  };
-
-  const deselectCorrection = () => {
-    setEditorSuggestions([]);
-    setCurrentCorrection(null);
-  };
-
-
-
-  const spellCheck = () => {
-    const text = editorRef.current.editor.getText();
+  // Mistake checking
+  const getMistakes = (text) => {
     const words = text.split(/[\n\s]/);
     return words.map((word) => {
       var cleanWord = word.replace(/[^a-zA-Z'-]/g, "");
@@ -96,73 +42,137 @@ const Editor = ({ editorRef, formattedValue, setFormattedValue }) => {
       }
     }).filter((word) => word);
   }
-
-  const getSuggestions = (correction) => {
-    const suggestions = spell.suggest(correction.word).slice(0, 5);
-    return suggestions;
+  const getMistakeFromIndex = (index) => {
+    for (var i = 0; i < spellCheckMistakes.length; i++) {
+      if (index >= spellCheckMistakes[i].start && index <= spellCheckMistakes[i].end) {
+        return spellCheckMistakes[i];
+      }
+    }
+    return null;
   }
-  const handleSuggestionClick = (event, suggestion) => {
-    event.preventDefault();
-    const correction = currentCorrection;
-    const text = editorRef.current.editor.getText();
-    const newText = text.substring(0, correction.start) + suggestion + text.substring(correction.end);
-    editorRef.current.editor.setText(newText, "silent");
-    deselectCorrection();
+
+  // Corrections
+
+  const getCorrectionsForMistake = (mistake) => {
+    const corrections = spell.suggest(mistake.word).slice(0, 5);
+    mistake.corrections = corrections;
+    return corrections;
+  }
+
+  // Highlighting
+
+  const clearHighlighting = () => {
+    editorRef.current.editor.removeFormat(
+      0,
+      editorRef.current.editor.getLength() - 1,
+      "silent"
+    );
+  };
+
+  const highlightMistakes = (mistakes) => {
     clearHighlighting();
-    setSpellCheckCorrections(spellCheck());
-    highlightCorrections(spellCheckCorrections);
+    mistakes.forEach((mistake) => {
+      editorRef.current.editor.formatText(
+        mistake.start,
+        mistake.end - mistake.start,
+        { color: "red" },
+        "silent"
+      );
+    });
+  };
+
+  const selectMistake = (mistake) => {
+    editorRef.current.editor.removeFormat(
+      mistake.start,
+      mistake.end - mistake.start,
+      "silent"
+    );
+    editorRef.current.editor.formatText(
+      mistake.start,
+      mistake.end - mistake.start,
+      { background: "red" },
+      "silent"
+    );
+    if (JSON.stringify(mistake) !== JSON.stringify(currentMistake)) {
+      setCurrentMistake(mistake);
+      setEditorCorrections(mistake.corrections || getCorrectionsForMistake(mistake));
+    }
+  };
+
+  const deselectMistake = () => {
+    setEditorCorrections([]);
+    setCurrentMistake(null);
+  };
+
+  // Event handlers
+
+  const handleSelectionChange = (selection) => {
+    const mistake = getMistakeFromIndex(selection.index);
+    highlightMistakes(spellCheckMistakes);
+    if (mistake != null) {
+      selectMistake(mistake);
+    } else {
+      deselectMistake();
+    }
+  };
+
+  const handleCorrectionClick = (event, correction) => {
+    event.preventDefault();
+    const mistake = currentMistake;
+    const text = editorRef.current.editor.getText();
+    const newText = text.substring(0, mistake.start) + correction + text.substring(mistake.end);
+    editorRef.current.editor.setText(newText, "silent");
+    editorRef.current.editor.setSelection(mistake.start + correction.length, 0, "silent");
+    deselectMistake();
+    const mistakes = getMistakes(newText);
+    setSpellCheckMistakes(mistakes);
+    highlightMistakes(mistakes);
   }
 
+  const handleEditorChange = (value, delta, source, editor) => {
+    setFormattedValue(value);
+    if (source === "user") {
+      const mistakes = getMistakes(editor.getText());
+      setSpellCheckMistakes(mistakes);
+      highlightMistakes(mistakes);
+    }
+  };
+
+  const handleEditorChangeSelection = (selection, source, editor) => {
+    if (source === "user") {
+      if (selection != null && selection.length === 0) {
+        handleSelectionChange(selection);
+      } else {
+        highlightMistakes(spellCheckMistakes);
+      }
+    }
+  };
 
   return (
     <>
       <div className="p-2 border rounded-md h-10">
-        {editorSuggestions.map((suggestion) => (
+        {editorCorrections.map((correction) => (
           <button
-            key={suggestion}
-            onClick={(event) => handleSuggestionClick(event, suggestion)}
+            key={correction}
+            onClick={(event) => handleCorrectionClick(event, correction)}
             className={`text-black py-1 px-2 rounded text-sm w-1/5`}
           >
-            {suggestion}
+            {correction}
           </button>
         ))}
       </div>
-      <div className="mb-6 flex-grow">
+      <div className="mb-6 h-full overflow-y-auto">
         <ReactQuill
           ref={editorRef}
           theme="snow"
           placeholder="Enter your text here"
           value={formattedValue}
-          className="w-full h-full border rounded-md resize-none text-lg"
+          className="w-full h-full border rounded-md text-lg"
           formats={
             ["color", "background"]
           }
-          onChange={(value, delta, source, editor) => {
-            if (source === "user" || source === "silent") {
-              const corrections = spellCheck();
-              setSpellCheckCorrections(corrections);
-            }
-            setFormattedValue(value);
-          }}
-          onChangeSelection={(selection, source, editor) => {
-            if (source === "user" || source === 'silent') {
-              if (selection != null && selection.length === 0) {
-                const correction = getSpellCheckCorrection(selection.index);
-                if (correction != null) {
-                  clearHighlighting();
-                  highlightCorrections(spellCheckCorrections);
-                  selectCorrection(correction);
-                } else {
-                  clearHighlighting();
-                  highlightCorrections(spellCheckCorrections);
-                  deselectCorrection();
-                }
-              } else {
-                clearHighlighting();
-                highlightCorrections(spellCheckCorrections);
-              }
-            }
-          }}
+          onChange={handleEditorChange}
+          onChangeSelection={handleEditorChangeSelection}
           modules={editorModules}
         />
       </div>
