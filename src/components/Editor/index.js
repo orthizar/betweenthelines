24e41ed2f1bb251f555d78be4008b8f62a5138ea
Silvan Kohler <silvankohler@proton.me>
2023-10-10 12:38:53 -0700

    add tab control to corrections

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -59,7 +59,26 @@ const Editor = ({ editorRef, formattedValue, setFormattedValue }) => {
     const corrections = spell.suggest(mistake.word).slice(0, 5);
     mistake.corrections = corrections;
     return corrections;
-  }
+  };
+
+  const navigateCorrections = () => {
+    setSelectedCorrection((prevSelectedCorrection) => prevSelectedCorrection + 1)
+  };
+
+  const applyCorrection = (correction) => {
+    console.log("apply correction", correction)
+    console.log("current mistake", currentMistake)
+    const mistake = currentMistake;
+    const text = editorRef.current.editor.getText();
+    const newText = text.substring(0, mistake.start) + correction + text.substring(mistake.start + correction.length);
+    editorRef.current.editor.setText(newText, "silent");
+    editorRef.current.editor.setSelection(mistake.start + correction.length, 0, "silent");
+    deselectMistake();
+    const mistakes = getMistakes(newText);
+    setSpellCheckMistakes(mistakes);
+    highlightMistakes(mistakes);
+    blinkCorrection(mistake, correction);
+  };
 
   // Highlighting
 
@@ -154,16 +173,7 @@ const Editor = ({ editorRef, formattedValue, setFormattedValue }) => {
 
   const handleCorrectionClick = (event, correction) => {
     event.preventDefault();
-    const mistake = currentMistake;
-    const text = editorRef.current.editor.getText();
-    const newText = text.substring(0, mistake.start) + correction + text.substring(mistake.start + correction.length);
-    editorRef.current.editor.setText(newText, "silent");
-    editorRef.current.editor.setSelection(mistake.start + correction.length, 0, "silent");
-    deselectMistake();
-    const mistakes = getMistakes(newText);
-    setSpellCheckMistakes(mistakes);
-    highlightMistakes(mistakes);
-    blinkCorrection(mistake, correction);
+    applyCorrection(correction);
   }
 
   const handleEditorChange = (value, delta, source, editor) => {
@@ -186,29 +196,32 @@ const Editor = ({ editorRef, formattedValue, setFormattedValue }) => {
     }
   };
 
+  const handleEditorTab = (range, context) => {
+    console.log("tab", range, context);
+    if (context.format.background && context.format.background === "red") {
+      navigateCorrections();
+      return false;
+    }
+    return true;
+  };
+
+  const handleEditorEnter = (range, context) => {
+    console.log("enter", range, context);
+    console.log(editorCorrections, selectedCorrection, currentMistake)
+    if (context.format.background && context.format.background === "red") {
+      applyCorrection(editorCorrections[selectedCorrection % editorCorrections.length]);
+      return false;
+    }
+    return true;
+  };
+
   // Editor configuration
 
   const editorModules = {
     toolbar: null,
   };
-  
+
   useEffect(() => {
-    const navigateCorrections = () => {
-      const newSelection = selectedCorrection === null ? 0 : (selectedCorrection + 1) % editorCorrections.length;
-      setSelectedCorrection(newSelection);
-      console.log(newSelection)
-    };
-
-    const handleEditorTab = (range, context) => {
-      console.log("tab", range, context);
-      if (context.format.background && context.format.background === "red") {
-        navigateCorrections();
-        return false;
-      }
-      console.log(context.format.background && context.format.background === "red");
-      return false;
-    };
-
     if (!editorBindingsInitialized) {
       editorRef.current.editor.keyboard.bindings[9].unshift(
         {
@@ -216,22 +229,26 @@ const Editor = ({ editorRef, formattedValue, setFormattedValue }) => {
           handler: handleEditorTab,
         },
       );
+      editorRef.current.editor.keyboard.bindings[13].unshift(
+        {
+          key: 13,
+          handler: handleEditorEnter,
+        },
+      );
       editorBindingsInitialized = true;
-      console.log(editorRef.current.editor.keyboard.bindings);
-
     }
-  }, [editorRef, setSelectedCorrection, selectedCorrection, editorCorrections]);
+  }, [editorRef, handleEditorEnter, handleEditorTab]);
 
   return (
     <>
       <div className="mb-2 p-2 border rounded-md h-14">
-        {editorCorrections.map((correction) => (
+        {editorCorrections.map((correction, index) => (
           <button
             key={correction}
             onClick={(event) => handleCorrectionClick(event, correction)}
             onMouseEnter={() => previewCorrection(correction)}
             onMouseLeave={() => unpreviewCorrection(correction)}
-            className={`text-black rounded text-sm w-1/5 ${selectedCorrection === correction ? "bg-gray-200" : "bg-white"}`}
+            className={`text-black rounded text-sm w-1/5 ${selectedCorrection % editorCorrections.length === index ? "bg-gray-200" : "bg-white"}`}
           >
             {correction}
           </button>
