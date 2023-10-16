e7a511df4bdc7d8a13947dfb6c00edf071a6516e
Silvan Kohler <silvankohler@proton.me>
2023-10-16 16:54:07 -0700

    improve initial suggestion

--- src/components/Helpers/learn.js
+++ src/components/Helpers/learn.js
@@ -4,7 +4,8 @@ import { getMaxTokens, suggestPrompt } from "./prompts";
 const maxRetries = 3;
 
 export const suggest = async (text, messages) => {
-  const prompt = suggestPrompt(text, messages);
+  const format = "email";
+  const prompt = suggestPrompt(text, messages, format);
   for (var retry = 0; retry < maxRetries; retry++) {
     try {
       const response = await invokeLLM(prompt, getMaxTokens(prompt));
