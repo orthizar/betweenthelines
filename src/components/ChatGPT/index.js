028bb9a4c60b33021c0a7c99a2f9d8c9c5c71604
Luca Mock <luca.mock@ringieraxelspringer.ch>
2023-10-17 10:15:05 -0700

    working background animation

--- src/components/ChatGPT/index.js
+++ src/components/ChatGPT/index.js
@@ -116,6 +116,21 @@ const ChatGPT = () => {
           </div>
         </div>
       </div>
+
+      <div className="area">
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,21 +7,7 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="area">
-      <ul className="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-      <App />
-    </div>
+    <App />
   </React.StrictMode>
 );
 
