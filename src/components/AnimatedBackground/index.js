4fde27a39a2fa2244345e1aa73e0e5b348f16060
Silvan Kohler <silvankohler@proton.me>
2023-10-18 18:40:29 -0700

    flow field background

--- src/components/AnimatedBackground/index.js
+++ src/components/AnimatedBackground/index.js
@@ -1,77 +1,88 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Component } from "react";
 import p5 from "p5";
 
-const AnimatedBackground = ({ child }) => {
-  const canvasRef = useRef(null);
-  const [isInit, setIsInit] = useState(false);
-
-  useEffect(() => {
-    if (!isInit) {
-      const sketch = (p) => {
-        const w = canvasRef.current.offsetWidth;
-        const h = canvasRef.current.offsetHeight;
-        let t = 0;
-        let n = 800;
-        let particles = [];
-        const renderer = p.createCanvas(w, h);
-        renderer.parent(canvasRef.current)
-        p.setup = function () {
-          p.stroke(0, 10);
-
-          for (var i = 0; i < n; i++) {
-            particles.push({
-              pos: p.createVector(p.random(w), p.random(h)),
-              vel: p.createVector(0, 0),
-              seed: i,
-            });
-          }
-        };
-
-        p.draw = function () {
-          particles.forEach(function (prtcl) {
-            ui.display(p, prtcl.pos, prtcl.vel);
-            ui.update(p, t, prtcl.pos, prtcl.vel, prtcl.seed);
-          });
-          t += 0.002;
-        };
-
-        const ui = {
-          display: function display(p, pos, vel) {
-            p.point(pos.x, pos.y);
-          },
-
-          update: function update(p, t, pos, vel, seed) {
-            const w = p.windowWidth;
-            const h = p.windowHeight;
-
-            pos.x = mod((pos.x + vel.x), w);
-            pos.y = mod((pos.y + vel.y), h);
-
-            var r = p5.Vector.fromAngle(p.noise(seed, t) * p.TWO_PI);
-            vel.x = r.x;
-            vel.y = r.y;
-
-            vel.add(flow(p, pos)).mult(3);
-          },
-        };
-      };
-
-      new p5(sketch);
-      setIsInit(true);
-    }
-  }, [isInit]);
-
-  return <div
-    className="h-full w-full absolute"
-    style={{ zIndex: "-1" }}
-    ref={canvasRef}
-  ></div>;
-};
 function mod(x, n) {
   return ((x % n) + n) % n;
 };
+
 function flow(p, pos) {
   let r = p.noise(pos.x / 100, pos.y / 100) * p.TWO_PI;
   return p5.Vector.fromAngle(r).mult(2);
-};
+}
+
+function display(p, pos, vel) {
+  p.point(pos.x, pos.y);
+}
+
+function update(p, t, pos, vel, seed, w, h) {
+
+  pos.x = mod((pos.x + vel.x), w);
+  pos.y = mod((pos.y + vel.y), h);
+
+  var r = p5.Vector.fromAngle(p.noise(seed, t) * p.TWO_PI);
+  vel.x = r.x;
+  vel.y = r.y;
+
+  vel.add(flow(p, pos)).mult(3);
+}
+
+class AnimatedBackground extends Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+  }
+
+
+  componentDidMount() {
+    const sketch = (p) => {
+      const w = this.canvasRef.current.offsetWidth;
+      const h = this.canvasRef.current.offsetHeight;
+      let t = 0;
+      let n = 10000;
+      let particles = [];
+      p.setup = function () {
+        p.createCanvas(w, h);
+        p.stroke(0, 10);
+
+        for (var i = 0; i < n; i++) {
+          particles.push({
+            pos: p.createVector(p.random(w), p.random(h)),
+            vel: p.createVector(0, 0),
+            seed: i,
+          });
+        }
+      };
+
+      p.draw = function () {
+        particles.forEach(function (prtcl) {
+          display(p, prtcl.pos, prtcl.vel);
+          update(p, t, prtcl.pos, prtcl.vel, prtcl.seed, w, h);
+        });
+        t += 0.01;
+      };
+    };
+    this.p5 = new p5(sketch, this.canvasRef.current);
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    this.p5.remove();
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize = () => {
+    const canvas = this.canvasRef.current;
+    const width = canvas.offsetWidth;
+    const height = canvas.offsetHeight;
+    this.p5.resizeCanvas(width, height);
+  };
+
+  render() {
+    return <div
+      className="absolute h-full w-full -z-50"
+      ref={this.canvasRef}
+    />;
+  }
+}
+
 export default AnimatedBackground;
